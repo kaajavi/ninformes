d753354c1ca9f9d1bd46f932c7211d034b5b461e
kaajavi <javierguignard@gmail.com>
2015-04-29 08:43:33 -0300

    ceros en el número de matrícula

--- escolar/templatetags/tg_listado.py
+++ escolar/templatetags/tg_listado.py
@@ -4,6 +4,9 @@ register = template.Library()
 
 @register.simple_tag
 def increment_counter(outer, inner, outer_loop_length):
-    return (outer + inner * outer_loop_length + inner * (outer_loop_length - 1)) + 1
+    num = (outer + inner * outer_loop_length + inner * (outer_loop_length - 1))
+    num = num +1
+    ret = str(num).rjust(3).replace(' ','0')
+    return ret
 
 
