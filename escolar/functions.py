744e231988236375fbc3228bd56d26f26bc84d22
kaajavi <javierguignard@gmail.com>
2015-07-06 17:30:49 -0300

    arreglo orden items-- duplicados

--- escolar/functions.py
+++ escolar/functions.py
@@ -1,19 +1,21 @@
 # -*- encoding: utf-8 -*-
 from escolar.models import Docente, Curso, Alumno, MatriculaAlumnado, Campo, MatriculaDocentes, SITUACION_DOCENTE, TIPO_MATRICULA_DOCENTE, ItemCampo
 
-def ordenarPorColor(items):
+def ordenarPorColor(items):    
     colores = ["FF3333", "32CD32", "6666FF", "FF3399", "FFFF33","E0E0E0",
                "FF8C00", "EE82EE", "FF4500", "BDB76B", "008000","008B8B",
                "778899","FFE4E1","B0E0E6","BDB76B"]
     
     retItems = []
     
-    for color in colores:
-        print color
+    items = list(items)
+    
+    for color in colores:        
         color = "#" + color
         for item in items:
             if (item.color.upper()==color.upper()):
                 retItems.append(item)
+                items.remove(item)
     
     return retItems
     
